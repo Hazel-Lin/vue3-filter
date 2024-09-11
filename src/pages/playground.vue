83b14a9a7cd37dc35e032b944a62627031e608df
linhuizi <529246402@qq.com>
2024-09-11 11:46:45 +0800

    feat: init

---
+++ src/pages/playground.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+const form = ref({
+  name: '',
+})
+const show = ref(true)
+</script>
+
+<template>
+  <el-dialog v-model="show" title="title">
+    <el-form :model="form">
+      <el-form-item label="name" prop="name">
+        <el-input v-model="form.name" />
+      </el-form-item>
+    </el-form>
+  </el-dialog>
+</template>
